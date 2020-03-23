a93d66516a97d088c3e0aa6d4dd32ccee3f9f9de
naveenthangella <naveenthangella@gmail.com>
2020-03-22 18:13:04 -0700

    variable sample

--- Variables/sample1.tf
+++ Variables/sample1.tf
@@ -3,8 +3,14 @@ variable "simple-string" {
   default = "Simple"
 }
 
+variable "simple-number" {
+  type = number
+  default = 100
+}
+
 resource "null_resource" "sample" {
   provisioner "local-exec" {
-    command = "echo -e ${var.simple-string}"
+    command = "echo -e String = ${var.simple-string}"
+    command = "echo -e Number = ${var.simple-number}"
   }
 }
