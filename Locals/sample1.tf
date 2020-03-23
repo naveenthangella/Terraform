2539d7ad8ba2e5c1cf31c8a80b71c984f7f3cc80
naveenthangella <naveenthangella@gmail.com>
2020-03-23 13:29:50 -0700

    output sample

--- Locals/sample1.tf
+++ Locals/sample1.tf
@@ -6,8 +6,13 @@ variable "sample1" {
   default = 100
 }
 
+variable "sample2" {
+  type = list
+  default = ["naveen","Tangela"]
+}
+
 locals {
-  sampleall = "${var.sample}-${var.sample1}"
+  sampleall = "${var.sample}-${var.sample1}-${var.sample2[1]}"
 }
 
 output "SAMPLE" {
