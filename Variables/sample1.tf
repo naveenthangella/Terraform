c34f0b1d1e8063a6bb5178c34dda5cf17fac23cb
naveenthangella <naveenthangella@gmail.com>
2020-03-22 23:34:16 -0700

    variable sample

--- Variables/sample1.tf
+++ Variables/sample1.tf
@@ -8,8 +8,24 @@ variable "simple-number" {
   default = 100
 }
 
+variable "simple-boolean" {
+  default = true
+}
+
+variable "simple-list" {
+  type = list
+  default = [111, "abc"]
+}
+
+variable "simple-map" {
+  type = map
+  default = {
+    first-name = "Naveen"
+    last-name = "Tangela"
+  }
+}
 resource "null_resource" "sample" {
   provisioner "local-exec" {
-    command = "echo -e \"String = ${var.simple-string} \n Number = ${var.simple-number}\""
+    command = "echo -e \"String = ${var.simple-string} \n Number = ${var.simple-number} \n list value = ${var.simple-list[0]} \n Name = ${var.simple-map["first-name"]}\""
   }
 }
