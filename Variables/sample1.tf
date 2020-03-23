b9daff2a4237ad303c8e9d2a43dcc3560bd6f567
naveenthangella <naveenthangella@gmail.com>
2020-03-23 12:58:48 -0700

    variable sample

--- Variables/sample1.tf
+++ Variables/sample1.tf
@@ -26,6 +26,6 @@ variable "simple-map" {
 }
 resource "null_resource" "sample" {
   provisioner "local-exec" {
-    command = "echo -e \" String = ${var.simple-string} \n Number = ${var.simple-number} \n list value = ${var.simple-list} \n Name = ${var.simple-map}\""
+    command = "echo -e \" String = ${var.simple-string} \n Number = ${var.simple-number} \n list value = ${var.simple-list[1]} \n Name = ${var.simple-map["last-name"]}\""
   }
 }
