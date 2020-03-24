869b33043039debaa89df9cf55fddc8f02535329
naveenthangella <naveenthangella@gmail.com>
2020-03-24 00:48:17 -0700

    sample instance

--- Project/studentapp/stage-01/ec2.tf
+++ Project/studentapp/stage-01/ec2.tf
@@ -9,4 +9,20 @@ resource "aws_instance" "web" {
   ami = var.ami_id
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0183b01f41709052a"]
+
+  provisioner "remote-exec" {
+    connection {
+      host = self.public_ip
+      user = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "cd /tmp",
+      "git clone https://github.com/naveenthangella/SHELL-SCRIPT.git",
+      "cd /SHELL-SCRIPT/project/studentapp",
+      "sh webapp.sh"
+
+    ]
+  }
 }
