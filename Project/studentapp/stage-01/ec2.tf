76f6225ed885f523fdf34deee759e4084119a718
naveenthangella <naveenthangella@gmail.com>
2020-03-24 00:27:39 -0700

    sample instance

--- Project/studentapp/stage-01/ec2.tf
+++ Project/studentapp/stage-01/ec2.tf
@@ -1,7 +1,12 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
 variable "ami_id" {
   default = "ami-0777ff5c030fe1d38"
 }
 resource "aws_instance" "web" {
   ami = var.ami_id
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0183b01f41709052a"]
 }
