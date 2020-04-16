2a97802a4cf7955277ff6d1831fec6ba540181f5
naveenthangella <naveenthangella@gmail.com>
2020-04-16 16:13:31 -0700

    sample instance to execute ansible playbook

---
+++ Project/studentapp/stage-02/ec2.tf
@@ -0,0 +1,26 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
+variable "ami_id" {
+  default = "ami-0777ff5c030fe1d38"
+}
+resource "aws_instance" "web" {
+  ami = var.ami_id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0183b01f41709052a","sg-0404201bfe4db9f05","sg-052d4431ac57068ba"]
+
+  provisioner "remote-exec" {
+    connection {
+      host = self.public_ip
+      user = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "yum install ansible -y",
+      "echo localhost >/tmp/hosts",
+      "ansible-pull -i /tmp/hosts -U https://github.com/naveenthangella/Ansible.git Playbooks/webapp.yml"
+    ]
+  }
+}
