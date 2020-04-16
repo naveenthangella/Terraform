ad4d59346311dd19559015177bc4e712423efb74
naveenthangella <naveenthangella@gmail.com>
2020-04-16 16:23:27 -0700

    sample instance to execute ansible playbook

--- Project/studentapp/stage-02/ec2.tf
+++ Project/studentapp/stage-02/ec2.tf
@@ -8,7 +8,7 @@ variable "ami_id" {
 resource "aws_instance" "web" {
   ami = var.ami_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0183b01f41709052a","sg-0404201bfe4db9f05","sg-052d4431ac57068ba"]
+  vpc_security_group_ids = ["sg-0183b01f41709052a","sg-0404201bfe4db9f05","sg-052d4431ac57068ba","sg-0e1737254ce68c0b4"]
 
   provisioner "remote-exec" {
     connection {
