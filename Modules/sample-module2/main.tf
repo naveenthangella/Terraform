7d6766aeb551f7d80a7764718e6ee1f31387f431
naveenthangella <naveenthangella@gmail.com>
2020-04-17 21:02:19 -0700

    sample modules to add and multiply two numbers and display.

--- Modules/sample-module2/main.tf
+++ Modules/sample-module2/main.tf
@@ -3,10 +3,19 @@ module "add" {
   a = var.a
   b = var.b
 }
+module "multiply" {
+  source = "./multiply"
+  a = var.a
+  b = var.b
+}
 
 variable "a" {}
 variable "b" {}
 
 output "addition" {
   value = module.add.add
+}
+
+output "multiplication" {
+  value = module.multiply.multiply
 }
