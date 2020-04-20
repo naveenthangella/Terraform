14e5f7d96b425d3417c722cd3684a87eb08aa8a9
naveenthangella <naveenthangella@gmail.com>
2020-04-19 18:02:45 -0700

    creating a subnet for all the availability zones

---
+++ Project/studentapp/final/network/subnet.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "subnets" {
+  count       = length(data.aws_availability_zones.available.names)
+  cidr_block  = cidrsubnet(var.VPC_CIDR, 3, count.index)
+  vpc_id      = aws_vpc.main.id
+  tags        = {
+    "Name"    = "${var.DEFAULT_TAGS["PROJECT_NAME"]}-${var.DEFAULT_TAGS["ENV"]}-${count.index}"
+  }
+}
