7b5659e750fa03d4d04546ec09ba32d6e7f2c98b
naveenthangella <naveenthangella@gmail.com>
2020-04-20 20:42:07 -0700

    creating an ec2 intance with a random availability zone and respective subnet. Peering connection

--- Project/studentapp/final/network/routetable.tf
+++ Project/studentapp/final/network/routetable.tf
@@ -31,7 +31,7 @@ resource "aws_route" "route-to-student" {
 }
 
 resource "aws_route" "igw-route" {
-  route_table_id            = aws_route_table.student-workstation.id
+  route_table_id            = aws_route_table.PRIVATE-student-workstation.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.igw.id
 }
